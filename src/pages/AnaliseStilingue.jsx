f21f912192c6e401f3b8a4039100794142355254
enzo.souto <enzo.ferrari.sd@gmail.com>
2025-02-18 17:24:05 -0300

    Alteração do link AnaliseStilingue

--- src/pages/AnaliseStilingue.jsx
+++ src/pages/AnaliseStilingue.jsx
@@ -7,7 +7,7 @@ const Stilingue = () => {
           title="Análise Demográfica" 
           width="100%" 
           height="1060" 
-          src="https://studio.stilingue.com.br/6048375534911488/5539833958760448/dashboard?public=1&date_range=202501010000%3A202501312359&dashboard=kZjChclZ9WpwzDbZxQhB" 
+          src="https://studio.stilingue.com.br/6048375534911488/5539833958760448/dashboard?public=1&date_range=202502120000:202502182359&dashboard=kZjChclZ9WpwzDbZxQhB" 
           frameborder="0" 
           allowFullScreen="true">
         </iframe>
