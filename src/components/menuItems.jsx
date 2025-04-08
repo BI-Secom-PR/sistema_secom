e0086a70c0f974404cf7d78d7fd5ff9498ededf7
enzo.souto <enzo.ferrari.sd@gmail.com>
2025-04-08 16:40:38 -0300

    Ocultar paginas

--- src/components/menuItems.jsx
+++ src/components/menuItems.jsx
@@ -9,37 +9,37 @@ export const getMenuItems = (isDarkMode) => [
   },
   {
     name: 'Campanhas Ativas',
-    icon: <LayoutDashboard size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
+    icon: <LayoutDashboard size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
     path: '/campanhas_ativas'
   },
   {
     name: 'Assuntos do Momento',
-    icon: <BarChart2 size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
+    icon: <BarChart2 size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
     path: '/trends'
   },
   {
     name: 'Análise Detalhada',
-    icon: <Clock size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
+    icon: <Clock size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
     path: '/powerbi'
   },
   {
     name: 'Análise Demográfica',
-    icon: <MapIcon size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
+    icon: <MapIcon size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
     path: '/demografico'
   },
-  {
-    name: 'Dashboard Performance',
-    icon: <BarChart3 size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
-    path: '/performance'
-  },
-  {
-    name: 'Comparativo Histórico',
-    icon: <Clock2 size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
-    path: '/comparativo_historico'
-  },
+  //{
+   // name: 'Dashboard Performance',
+   // icon: <BarChart3 size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
+  //  path: '/performance'
+  //},
+  //{
+   // name: 'Comparativo Histórico',
+   // icon: <Clock2 size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
+   // path: '/comparativo_historico'
+  //},
   {
     name: 'Debate Digital',
-    icon: <ChartBar size={24} color={isDarkMode ? "#ffffff" : "#666"} />, // Cor dinâmica
+    icon: <ChartBar size={24} color={isDarkMode ? "#ffffff" : "#000"} />, // Cor dinâmica
     path: '/stilingue'
   }
 ];
